3c71ca76dbb501af547d46a4b0374b52d582e705
Hiroki Sakamoto <taisho6339@Hiroki-no-MacBook-Pro.local>
2014-07-23 23:24:33 +0900

    add createPage

--- date_matcher/date_matcher/models.py
+++ date_matcher/date_matcher/models.py
@@ -1,3 +1,5 @@
+# coding: UTF-8
+
 from sqlalchemy import (
     Column,
     Index,
@@ -18,10 +20,33 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+#イベントテーブル
+class Event(Base):
+    __tablename__ = 't_events'
+    _id = Column(Integer,primary_key=True)
+    name = Column(Text , nullable=False)
+    hash_id = Column(Text, nullable=False)
+    created_at = Column(Text, nullable=False)
+    completed_at = Column(Text)
+    status = Column(Integer, default=1)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+#ユーザテーブル,イベントに紐付く
+class User(Base):
+    __tablename__ = "m_users"
+    _id = Column(Integer,primary_key=True,autoincrement=True)
+    name = Column(Text , nullable=False)
+    event_id = Column(Integer , nullable=False)
+
+
+#日にちテーブル,ユーザの一日の状態を示す
+class Date(Base):
+    __tablename__ = "t_dates"
+    _id = Column(Integer , primary_key=True, autoincrement=True)
+    date = Column(Text , nullable=False)
+    user_id = Column(Integer , nullable=False)
+    event_id = Column(Integer , nullable=False)
+    status = Column(Integer , default=1)
+
+
+# Index('my_index', MyModel.name, unique=True, mysql_length=255)
--- date_matcher/date_matcher/scripts/initializedb.py
+++ date_matcher/date_matcher/scripts/initializedb.py
@@ -13,8 +13,10 @@ from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    MyModel,
     Base,
+    Event,
+    User,
+    Date,
     )
 
 
@@ -35,6 +37,6 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+    # with transaction.manager:
+        # model = Use(name='one', value=1)
+        # DBSession.add(model)
