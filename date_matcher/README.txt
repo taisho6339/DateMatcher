fcb5260e99e6ad153a7f35f74c57dcc52e4c63b4
Hiroki Sakamoto <taisho6339@Hiroki-no-MacBook-Pro.local>
2014-07-23 10:10:33 +0900

    add files

---
+++ date_matcher/README.txt
@@ -0,0 +1,14 @@
+date_matcher README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/populate_date_matcher development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ date_matcher/setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='date_matcher',
+      version='0.0',
+      description='date_matcher',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='date_matcher',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = date_matcher:main
+      [console_scripts]
+      initialize_date_matcher_db = date_matcher.scripts.initializedb:main
+      """,
+      )
+
